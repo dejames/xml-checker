c010f679dc907120d76120795a837b68b71fa707
Deepu James <v-dejames@egencia.com>
2016-03-15 10:45:33 +0530

    separate exception catching
    Signed-off-by: Deepu James <v-dejames@egencia.com>

--- src/ComparisonTest.java
+++ src/ComparisonTest.java
@@ -2,8 +2,10 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.nio.file.Files;
@@ -16,6 +18,7 @@ import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
@@ -68,7 +71,13 @@ public class ComparisonTest {
                 System.out.println("Size of Hash list:\t\t " + hashSetOfXmls.size());
                 System.out.println("Size of Duplicate list:\t " + listOfDuplicates.size());
             }
-        } catch (Exception e) {
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
